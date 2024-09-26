f0fb2221ca7c41d7bc284799b3f8af741fa5e207
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-26 15:34:13 +0300

    updated: room - accessing database

--- app/src/main/java/com/ismailmesutmujde/kotlinroomlibrary/view/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinroomlibrary/view/MainActivity.kt
@@ -3,12 +3,21 @@ package com.ismailmesutmujde.kotlinroomlibrary.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.ismailmesutmujde.kotlinroomlibrary.R
+import com.ismailmesutmujde.kotlinroomlibrary.dao.PersonsDao
+import com.ismailmesutmujde.kotlinroomlibrary.database.DatabaseAccess
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var db:DatabaseAccess
+    private lateinit var pdao:PersonsDao
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        db = DatabaseAccess.databaseAccess(this)!!
+        pdao = db.getPersonsDao()
 
     }
 }
